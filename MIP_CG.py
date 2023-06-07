0e97f062b731ef75d4b8c749cc1c6eff8c8da245
Mehdi <el-mehdi.oudaoud@polymtl.ca>
2023-06-07 16:16:37 -0400

    Constraints Construction

--- MIP_CG.py
+++ MIP_CG.py
@@ -12,11 +12,49 @@ from numpy.typing import NDArray
 
 dim = int(3)
 
+Square = tuple(int,int,int,int)
+
+
 def extract_residuals (loop : Loop ):
-    l = []
-    for k in loop:
-        l.append(k.res.pos)
-    return l
+    lasso_positions = []
+    for r in loop:
+        pos = [r.res.ax] + list(r.res.pos)
+        if pos[0] == 2:
+            new_pos = [pos[1]+0.5,pos[2] + 0.5,pos[3]]
+        elif pos[0] == 1:
+            new_pos = [pos[1]+0.5,pos[2],pos[3]+0.5]
+        elif pos[0] == 0:
+            new_pos = [pos[1],pos[2] + 0.5,pos[3]+0.5]
+        lasso_positions.append(new_pos)
+    return np.array(lasso_positions) 
+
+
+def defined_cage(loop : Loop):
+    positions = extract_residuals(loop)
+    dict = {}
+
+    return dict
+
+def closed_paths_2D(carre : Square) -> list[Square]:
+    "Gives the closed paths that are derived from a 2D square"
+    x1,x2,y1,y2 = carre
+    debut = np.array([x1,x2,y1,y2])
+    assert(x2-x1 == y2-y1)
+    L = []
+    for step in range(x2 - x1):
+        L.append(debut + step*np.array([1,0,1,0]))
+        L.append(debut + step*np.array([0,-1,0,-1]))
+    return L
+
+
+
+
+    
+
+
+
+
+
 
 
 
