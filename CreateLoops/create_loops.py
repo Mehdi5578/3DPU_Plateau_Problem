eda39c157ee51045581be572e3805885e84c037d
Mehdi5578 <oudaoud.mehdi@gmail.com>
2023-06-14 14:31:20 -0400

    create script loops

--- CreateLoops/create_loops.py
+++ CreateLoops/create_loops.py
@@ -1,14 +1,14 @@
 import yaml
 import sys
 import nibabel as nb
-from _3dpu import *
+from _3DLoops._3dpu import *
 
 
 
 Initial_Path = sys.path[-1]
 
-with open(Initial_Path + '\\paths\\MRI_data.yaml', 'r') as file :
-    paths = yaml.safe_load(file)
+with open(Initial_Path + '\\paths\\MRI_data.yaml', 'r') as fi :
+    paths = yaml.safe_load(fi)
 
 chemin = paths["Paths"]["phase"].decode('utf-8').encode('cp1252')
 t = paths["Limits"]["t"]
@@ -18,4 +18,5 @@ print(chemin)
 if __name__ == '__main__':
     phase_image = nb.load(chemin)
     phase_data = phase_image.get_data()[:,:,:,t]
+    
 
