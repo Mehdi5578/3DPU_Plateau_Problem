6bd95d96e2ec8a61bca97410199852bcc586be6a
Mehdi <el-mehdi.oudaoud@polymtl.ca>
2023-09-06 12:24:25 -0400

    commit

--- Plateau Problem/W_n.py
+++ Plateau Problem/W_n.py
@@ -2,9 +2,9 @@ from scipy.integrate import dblquad  # Module d'intégration "dblquad"
 import numpy as np
 from tqdm import tqdm
 import pyaml
-from pyaml.loader import SafeLoader
+from yaml.loader import SafeLoader
 
-with open('paths\MRI_data.yaml', 'r') as f:
+with open(r"C:\Users\oudao\OneDrive\Documents\Montréal 4A\Les études\Chair_SCALE_AI\3dPU\paths\MRI_data.yaml", 'r') as f:
     data = yaml.load(f, Loader=SafeLoader)
 
 
@@ -13,30 +13,31 @@ ax = 0
 bx = 2*np.pi
 ay = 0
 by = 2*np.pi
-N = data[]
+N = 100
+A = (np.linspace(0,2*np.pi,N))
 
 
 W_N = np.zeros((N,N))
   # Fonction à intégrer
 for i in tqdm(range(N)):
-  for j in tqdm(range(N-1)):
+  for j in tqdm(range(N)):
     def func(x, y):
         ph_i = 0
         ph_j = 0
 
-        if x < A[i] and A[i-1] <= x :
+        if x < A[i%N] and A[(i-1)%N] <= x :
           ph_i = N/(2*np.pi)
-        elif x < A[i+1] and A[i] <= x :
+        elif x < A[(i+1)%N] and A[i%N] <= x :
           ph_i = -N/(2*np.pi)
 
-        if y < A[j] and A[j-1] <= y :
+        if y < A[j%N] and A[(j-1)%N] <= y :
           ph_j = N/(2*np.pi)
-        elif y < A[j+1] and A[j] <= y :
+        elif y < A[(j+1)%N] and A[j%N] <= y :
           ph_j = -N/(2*np.pi)
 
 
 
-        return np.log((np.sin(np.abs(x-y)/2)) )*ph_i*ph_j
+        return np.log((np.sin(np.abs(x-y)/2)) + 0.0000000001 )*ph_i*ph_j
 
 
     # Calcul de l'intégrale
