7be06f881527c1df5db15194854c986eca92b666
Mehdi <el-mehdi.oudaoud@polymtl.ca>
2023-09-13 16:17:37 -0400

    W_N correction

--- Plateau Problem/W_n.py
+++ Plateau Problem/W_n.py
@@ -14,7 +14,7 @@ bx = 2*np.pi
 ay = 0
 by = 2*np.pi
 N = data["Plateau_meta"]["N"]
-A = (np.linspace(0,2*np.pi,N+1))
+A = (np.linspace(0,2*np.pi,N+19))
 h = 2*np.pi/N
 
 W_N = np.zeros((N,N))
@@ -25,31 +25,15 @@ for i in tqdm(range(N)):
         ph_i = 0
         ph_j = 0
 
-        ph_i = 0
-        x_i = A[i]
-        x_i_ = x_i
-        x_im = A[(i-1)]
-        x_ip = A[(i+1)]
-        if i == 0 : 
-            x_i, x_ip, x_im  = A[0],A[1],A[-2]
-            x_i_ = A[-1]
-        if x < x_i_ and x_im <= x :
-            ph_i = -N/(2*np.pi)
-        elif x < x_ip and x_i <= x :
-            ph_i = N/(2*np.pi)
+        if x < A[i%N] and A[(i-1)%N] <= x :
+          ph_i = -N/(2*np.pi)
+        elif x < A[(i+1)%N] and A[i%N] <= x :
+          ph_i = N/(2*np.pi)
 
-        ph_i = 0
-        x_j = A[j]
-        x_j_ = x_j
-        x_jm = A[(j-1)]
-        x_jp = A[(j+1)]
-        if j == 0 : 
-            x_j, x_jp, x_jm  = A[0],A[1],A[-2]
-            x_j_ = A[-1]
-        if x < x_j_ and x_jm <= x :
-            ph_j = -N/(2*np.pi)
-        elif x < x_jp and x_j <= x :
-            ph_j = N/(2*np.pi)
+        if y < A[j%N] and A[(j-1)%N] <= y :
+          ph_j = -N/(2*np.pi)
+        elif y < A[(j+1)%N] and A[j%N] <= y :
+          ph_j = N/(2*np.pi)
 
 
 
