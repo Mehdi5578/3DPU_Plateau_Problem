805057fd0243bc1fd56a5ad2caf512ee3d8b99c0
Mehdi <el-mehdi.oudaoud@polymtl.ca>
2023-09-13 14:27:12 -0400

    manual

--- Plateau Problem/W_n.py
+++ Plateau Problem/W_n.py
@@ -14,7 +14,7 @@ bx = 2*np.pi
 ay = 0
 by = 2*np.pi
 N = data["Plateau_meta"]["N"]
-A = (np.linspace(0,2*np.pi,N))
+A = (np.linspace(0,2*np.pi,N+1))
 h = 2*np.pi/N
 
 W_N = np.zeros((N,N))
@@ -25,20 +25,35 @@ for i in tqdm(range(N)):
         ph_i = 0
         ph_j = 0
 
-        if x < A[i%N] and A[(i-1)%N] <= x :
-          ph_i = -N/(2*np.pi)
-        elif x < A[(i+1)%N] and A[i%N] <= x :
-          ph_i = N/(2*np.pi)
+        ph_i = 0
+        x_i = A[i]
+        x_i_ = x_i
+        x_im = A[(i-1)]
+        x_ip = A[(i+1)]
+        if i == 0 : 
+            x_i, x_ip, x_im  = A[0],A[1],A[-2]
+            x_i_ = A[-1]
+        if x < x_i_ and x_im <= x :
+            ph_i = -N/(2*np.pi)
+        elif x < x_ip and x_i <= x :
+            ph_i = N/(2*np.pi)
 
-        if y < A[j%N] and A[(j-1)%N] <= y :
-          ph_j = -N/(2*np.pi)
-        elif y < A[(j+1)%N] and A[j%N] <= y :
-          ph_j = N/(2*np.pi)
+        ph_i = 0
+        x_j = A[j]
+        x_j_ = x_j
+        x_jm = A[(j-1)]
+        x_jp = A[(j+1)]
+        if j == 0 : 
+            x_j, x_jp, x_jm  = A[0],A[1],A[-2]
+            x_j_ = A[-1]
+        if x < x_j_ and x_jm <= x :
+            ph_j = -N/(2*np.pi)
+        elif x < x_jp and x_j <= x :
+            ph_j = N/(2*np.pi)
 
 
 
-        return ph_i*ph_j
-        
+        return np.log((np.sin(np.abs(x-y)/2)) + 0.0000000001 )*ph_i*ph_j
 
 
     # Calcul de l'intégrale
