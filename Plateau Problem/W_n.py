accff2b4edabc4fd3068d39b3403a52427ac0a3c
Mehdi <el-mehdi.oudaoud@polymtl.ca>
2023-09-13 13:20:32 -0400

    manual integration

--- Plateau Problem/W_n.py
+++ Plateau Problem/W_n.py
@@ -1,7 +1,7 @@
 from scipy.integrate import dblquad  # Module d'intégration "dblquad"
 import numpy as np
 from tqdm import tqdm
-import pyaml
+import yaml
 from yaml.loader import SafeLoader
 
 with open(r"C:\Users\oudao\OneDrive\Documents\Montréal 4A\Les études\Chair_SCALE_AI\3dPU\paths\MRI_data.yaml", 'r') as f:
@@ -13,9 +13,9 @@ ax = 0
 bx = 2*np.pi
 ay = 0
 by = 2*np.pi
-N = 100
+N = data["Plateau_meta"]["N"]
 A = (np.linspace(0,2*np.pi,N))
-
+h = 2*np.pi/N
 
 W_N = np.zeros((N,N))
   # Fonction à intégrer
@@ -26,18 +26,19 @@ for i in tqdm(range(N)):
         ph_j = 0
 
         if x < A[i%N] and A[(i-1)%N] <= x :
-          ph_i = N/(2*np.pi)
-        elif x < A[(i+1)%N] and A[i%N] <= x :
           ph_i = -N/(2*np.pi)
+        elif x < A[(i+1)%N] and A[i%N] <= x :
+          ph_i = N/(2*np.pi)
 
         if y < A[j%N] and A[(j-1)%N] <= y :
-          ph_j = N/(2*np.pi)
-        elif y < A[(j+1)%N] and A[j%N] <= y :
           ph_j = -N/(2*np.pi)
+        elif y < A[(j+1)%N] and A[j%N] <= y :
+          ph_j = N/(2*np.pi)
 
 
 
-        return np.log((np.sin(np.abs(x-y)/2)) + 0.0000000001 )*ph_i*ph_j
+        return ph_i*ph_j
+        
 
 
     # Calcul de l'intégrale
---
+++ Plateau Problem/W_n_manual.py
@@ -0,0 +1,36 @@
+import numpy as np
+import yaml
+from yaml.loader import SafeLoader
+
+with open(r"C:\Users\oudao\OneDrive\Documents\Montréal 4A\Les études\Chair_SCALE_AI\3dPU\paths\MRI_data.yaml", 'r') as f:
+    data = yaml.load(f, Loader=SafeLoader)
+
+# N = data["Plateau_meta"]["N"]
+N = 5
+h = np.pi*2/N
+
+def R(i,j,k):
+    return 2*np.cos(k*h*(i-j))  - np.cos(k*h*(i-j-1)) - np.cos(k*h*(i-j+1))
+
+
+def D(i,j) : 
+    S = 0
+    for k in range(1,1000):
+        S += 2*R(i,j,k)/(k*k*k)
+    return np.log(2)*(h**2) - S
+
+
+def S(i,j) :
+    return D(i,j+1)/h - D(i,j)/h
+
+
+def W(i,j):
+    return (S(i,j) - S(i+1,j))/(4*h*np.pi)
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
