3c382daeeeb29c5c39b0ede04ec939d25e213091
Mehdi <el-mehdi.oudaoud@polymtl.ca>
2023-09-25 13:29:30 -0400

    finish initial traiangulation

--- Plateau Problem/Triangulation Meshing/Initial_triangulation.py
+++ Plateau Problem/Triangulation Meshing/Initial_triangulation.py
@@ -1,38 +1,72 @@
 from PointList import *
 
 class TriangularMesh:
-    def __init__(self, boundary, desired_triangle_count):
-        self.boundary = boundary
-        self.desired_triangle_count = desired_triangle_count
+    def __init__(self, boundary : PointList, desired_triangle_count):
+        self.boundary = boundary 
+        self.m = desired_triangle_count
         self.mesh = []
+        self.n = len(boundary.points)
+        
         
 
     def compute_central_point(self):
-        #  Compute the central point by averaging the vertices of the boundary
-        pass
+        return self.boundary.average_point()
+        
         
 
     def create_initial_subdivisions(self):
-        # TODO: Create initial subdivisions using the central point
-        pass
+        #  Create initial subdivisions using the central point
+        C = self.compute_central_point()
+        s = int(self.m/(2*self.n) - 1/2)
+        triangle = self.boundary.points + [self.boundary.points[0]]
+        P = [triangle]
+        for j in range(1,s+1):
+            P_j = triangle + (j / (s+1))*(C - triangle)
+            P.append(P_j)
+        
+        return np.array(P)
+        
 
-    def split_quadrilaterals(self):
-        # TODO: Split any quadrilaterals into triangles
-        pass
+    def create_quadrilaterals(self):
+        #split the outside quadrilaterals
+        P = (self.create_initial_subdivisions())
+        # P.append(P[0])
+        # P = np.array(P)
+        s = int(self.m/(2*self.n) - 1/2)
+        for j in tqdm(range(s)) : 
+            for i in range(self.n) :
+                self.mesh.append([P[j,i],P[j+1,i+1],P[j,i+1]])
+                self.mesh.append([P[j,i],P[j+1,i+1],P[j+1,i]])
+        
+        
+
+
+    def split_quadrilateral(self):
+        C = self.compute_central_point()
+        P = self.create_initial_subdivisions()
+        for i in tqdm(range(self.n)) :
+            self.mesh.append([C,P[-1,i],P[-1,i+1]])
 
     def further_subdivide(self):
-        # TODO: Further subdivide triangles if the count doesn't match the desired amount
-        pass
+        while len(self.mesh) < self.m :
+            k = np.random.randint(0,len(self.mesh))
+            tri = np.array(self.mesh.pop(k))
+            C = np.mean(tri,axis = 0)
+            tri1 = [tri[0],tri[1],C]
+            tri2 = [tri[0],tri[2],C]
+            tri3 = [tri[1],tri[2],C]
+            self.mesh += [tri1,tri2,tri3]
+    
+
+
 
     def generate_mesh(self):
-        self.compute_central_point()
-        self.create_initial_subdivisions()
+        self.create_quadrilaterals()
         self.split_quadrilaterals()
         self.further_subdivide()
         return self.mesh
 
 
-C = PointList.PointList()
 # # Usage example:
 # if __name__ == "__main__":
 #     boundary = [(x1, y1), (x2, y2), ...]  # Define the boundary vertices here
