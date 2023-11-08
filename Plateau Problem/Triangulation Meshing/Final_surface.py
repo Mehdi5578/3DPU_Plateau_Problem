c22a730cdf9164322136e89bbcc9e4fb5ae30de3
Mehdi <el-mehdi.oudaoud@polymtl.ca>
2023-11-08 15:50:16 -0500

    update the algorithm

--- Plateau Problem/Triangulation Meshing/Final_surface.py
+++ Plateau Problem/Triangulation Meshing/Final_surface.py
@@ -4,47 +4,92 @@ from copy import *
 class Edge_Flipping(Updating_Laplace):
     def __init__(self, boundary : PointList, desired_triangle_count):
         super().__init__(boundary, desired_triangle_count)
+        self.dict_edges = dict()
+        self.L = {}
+        self.L_replaced = {}
 
     def fill_edges(self):
+        self.edges = set()
         for tri in self.triangles:
             for i in range(3):
                 edge = tuple(sorted([tri[i], tri[(i+1)%3]]))
                 self.edges.add(edge)
+                self.dict_edges[edge] = -1
 
     def test(self,edge,tri):
         return set(edge).issubset(set(tri))
     
     def can_flip(self,edge):
-        triangles = [triangle for triangle in self.triangles if self.test(edge,triangle)]
-        return len(triangles) == 2
+        triangles = [tuple(set(sorted(triangle)))  for triangle in self.triangles if self.test(edge,triangle)]
+        return len(set(triangles)) == 2
     
     def flip_edge(self,edge):
-        triangles = [triangle for triangle in self.triangles if self.test(edge,triangle)]
-        t1,t2 = triangles
+
+        old_triangles = [triangle for triangle in self.triangles if self.test(edge,triangle)]
+        t1,t2 = old_triangles
         opposite_vertices = [v for v in t1 if v not in edge] + [v for v in t2 if v not in edge]
-        new_triangles = [[edge[0], opposite_vertices[0], opposite_vertices[1]], 
-                     [edge[1], opposite_vertices[0], opposite_vertices[1]]]
-        for triangle in triangles:
-            self.triangles.remove(triangle)
-        self.triangles.extend(new_triangles)
-        return triangles,new_triangles
+        new_triangles = [tuple(sorted([edge[0], opposite_vertices[0], opposite_vertices[1]])), 
+                     tuple(sorted([edge[1], opposite_vertices[0], opposite_vertices[1]]))]
+        return old_triangles,new_triangles
     
     def lawson_flip(self):
-       
-        for edge in self.edges:
-            if self.can_flip(edge):
-                triangles = [triangle for triangle in self.triangles if self.test(edge,triangle)]
-                old_area = sum(self.area_3D(triangle) for triangle in triangles)
-                old_tr, new_tr = self.flip_edge(edge)
-                new_area = sum(self.area_3D(triangle) for triangle in triangles)
 
-                if new_area >=  old_area: 
-                    for triangle in new_tr:
-                        self.triangles.remove(triangle)
-                    self.triangles.extend(old_tr) # Flip back if the new area is not smaller
+        swaped = True
+        ll = len(self.edges)
 
+        while swaped :
+            
+            swaped = False
 
-        
+            for edge in (self.edges):
+                if self.can_flip(edge):
+
+                    old_tr, new_tr = self.flip_edge(edge)
+                    old_area = sum(self.area_3D(triangle) for triangle in old_tr)
+                    new_area = sum(self.area_3D(triangle) for triangle in new_tr)
+                    
+                    R = set(np.array(new_tr).reshape(-1))
+                    edge1 = tuple(sorted([a for a in R if a not in edge]))
+                    
+                    if new_area < old_area and edge1 not in self.edges :
+                        # print("this edge.{} was swapped with this one {} resulting in an area minimization of {}".format(edge,edge1,old_area-new_area))
+                        # D = D + new_area - old_area
+                        # print("hat")
+                        
+                        swaped = True
+
+                        self.edges.add(edge1)
+                        self.edges.remove(edge)
+
+                        for tr in new_tr:
+                            self.triangles.append(tr)
+                        
+                        for tr in old_tr :
+                            self.triangles.remove(tr)
+
+                        for tr in new_tr:
+                            for pt in tr:
+                                self.dict_vertexes[pt].append(tuple(sorted(tr)))
+                        
+                        for tr in old_tr:
+                            for pt in tr:
+                                self.dict_vertexes[pt].remove(tuple(sorted(tr)))
+
+                        
+                        a,b = edge
+                        a1,b1 = edge1
+
+                        self.N[a1].append(b1)
+                        self.N[b1].append(a1)
+
+                        self.N[a].remove(b)
+                        self.N[b].remove(a)  
+                                              
+                    self.L[edge] = (new_area -  old_area, old_tr, new_tr,edge)
+                    if len(list(self.edges)) < ll:
+                        print(edge,edge1)
+                
+
 
         
 
--- Plateau Problem/Triangulation Meshing/Flairing_Laplace.py
+++ Plateau Problem/Triangulation Meshing/Flairing_Laplace.py
@@ -1,6 +1,8 @@
 from Initial_triangulation import TriangularMesh
 from PointList import *
 from tqdm import tqdm
+import math
+
 
 
 
@@ -8,12 +10,39 @@ class Updating_Laplace(TriangularMesh):
     def __init__(self, boundary : PointList, desired_triangle_count):
         super().__init__(boundary, desired_triangle_count)
 
+    def S(self,i,j):
+        S = 0
+        tr_i = set(self.dict_vertexes[i])
+        tr_j = set(self.dict_vertexes[j])
+        intersect = list(tr_i.intersection(tr_j))
+        if len(intersect) != 2:
+            print(intersect)
+            raise Exception("Sorry, it should have only two adjacent triangles")
+        for tr in intersect :
+            S+= self.area_3D(tr)
+        return S
+
+
+    def calcul_weights(self,i,j) :
+        S = 0
+        for k in self.N[i]:
+            S += (self.S(i,k))
+        
+        self.w[i,j] =  (self.S(i,j))/S
+        if math.isnan(self.w[i,j]):
+            print(i,j)
+            raise ValueError("S is nega tive")
+
+
+         
+
+
     def update_weights(self):
         "Updates the weights of the Mesh for each iteration"
-        for i in tqdm(range(self.N_vertexes)):
-            for j in (range(self.N_vertexes)):
-                if i in self.N[j] :
-                    self.calcul_weights(i,j)
+        for j in self.inside_indexes:
+            for i in self.N[j] :
+                self.calcul_weights(j,i)
+                
 
     def calculate_area(self):
         "Calculates the area of the meshing"
@@ -27,14 +56,15 @@ class Updating_Laplace(TriangularMesh):
         N_i = self.N[i]
         P = np.array([0,0,0])
         for k in N_i :
-            P = P + self.w[i,k]*np.array(self.mapping[k])
-        print(P)
+            P = P + (self.w[i,k])*np.array(self.mapping[k])
+        
         self.mapping[i] = P
-    
-    
 
-    
+        for k in N_i:
+            self.calcul_weights(i,k)
 
+            
+    
         
 
 
