c26f76be492c6b4ac0eb2d951b790e156b8d8a8a
Mehdi5578 <el.oudaoud@polytechnique.edu>
2024-01-09 13:43:49 -0800

    C++ MIP constraints

--- MIP constraints/MIP_model.cpp
+++ MIP constraints/MIP_model.cpp
@@ -1,23 +1,96 @@
 #include "MIP_model.hpp"
 #include <iostream>
 #include <tuple>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+#include <chrono>
 
 
-struct KeyHash {
-    std::size_t operator()(const std::tuple<int, int, int>& key) const {
-        const auto& [x, y, z] = key;
-        return std::hash<int>()(x) ^ std::hash<int>()(y) ^ std::hash<int>()(z);
-    }
-};
+
 
 // Constructor: initializes the number of vertices and resizes the adjacency list
-Graph::Graph(int vertices) : adjList(vertices) {}
+Graph::Graph(int N, int M, int L) {
+    for (int i = 0; i < N * M * L; ++i) {
+        adjList[i]; // Create an empty adjacency list for each vertex
+    }
+    std::cout << "Graph created" << std::endl;
+    CreateMapping(N,M,L);
+    std::cout << "Mapping created" << std::endl;
+}
 
+
+// from N*M*L create a mapping of the 3D points
+void Graph::CreateMapping(int N,int M, int L){
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
+            for (int k=0; k<L; k++){
+                int key = i*M*L + j*L + k;
+                mapping[key] = key;
+                
+            }
+        }
+    }        
+}
+
+std::tuple<int,int,int> Graph::getCoordinates(int index, int N, int M, int L){
+    int i = index/(M*L);
+    int j = (index-i*M*L)/L;
+    int k = index-i*M*L-j*L;
+    return std::make_tuple(i,j,k);
+}
+
+
+int Graph::getIndex(int i, int j, int k, int N, int M, int L){
+    return i*M*L + j*L + k;
+}
 
 void Graph::addEdge(int src, int dest) {
-    adjList[src].push_back(dest);
-    adjList[dest].push_back(src); // Comment this line for a directed graph
+    // add an edge to list adjacency from src to dest only if the edge does not already exist
+   adjList[src].push_back(dest);
+
+
+    }
+
+
+
+// from the mapping of the 3D points create the graph
+void Graph::CreateGraph(int N,int M, int L){
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
+            for (int k=0; k<L; k++){
+                int index = mapping[Graph::getIndex(i,j,k,N,M,L)];
+                if (i>0){
+                    int index2 = mapping[Graph::getIndex(i-1,j,k,N,M,L)];
+                    addEdge(index,index2);
+                }
+                if (i<N-1){
+                    int index2 = mapping[Graph::getIndex(i+1,j,k,N,M,L)];
+                    addEdge(index,index2);
+                }
+                if (j>0){
+                    int index2 = mapping[Graph::getIndex(i,j-1,k,N,M,L)];
+                    addEdge(index,index2);
+                }
+                if (j<M-1){
+                    int index2 = mapping[Graph::getIndex(i,j+1,k,N,M,L)];
+                    addEdge(index,index2);
+                }
+                if (k>0){
+                    int index2 = mapping[Graph::getIndex(i,j,k-1,N,M,L)];
+                    addEdge(index,index2);
+                }
+                if (k<L-1){
+                    int index2 = mapping[Graph::getIndex(i,j,k+1,N,M,L)];
+                    addEdge(index,index2);
+                }
+            }
+        }
+    }
 }
+
+
 
 // Performs BFS on the graph starting from vertex 'startVertex'
 void Graph::BFS(int startVertex) {
@@ -40,4 +113,64 @@ void Graph::BFS(int startVertex) {
             }
         }
     }
+}
+
+
+void Graph::printGraph() {
+    std::cout << " " << std::endl;
+    int index = 0;
+    for (int i=0; i<adjList.size(); i++){
+        std::cout << "Vertex " << i << " is connected to: ";
+        for (int j=0; j<adjList[i].size(); j++){
+            std::cout << adjList[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// void Graph::printMapping() {
+//     for (auto row : mapping) {
+//         std::cout << "Vertex " << row.first << " is mapped to: ";
+//         std::cout << row.second << " ";
+//         std::cout << std::endl;
+//     }
+// }
+
+int main() {
+    // Create a graph given in the above diagram
+    auto start = std::chrono::high_resolution_clock::now();
+    Graph g(100,100,100);
+    g.CreateGraph(100,100,100);
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = stop - start;
+    std::cout << "Time to create the graph: " << elapsed.count() << " s\n";
+    int cpt_3 = 0;
+    int cpt_4 = 0;
+    int cpt_5 = 0;
+    int cpt_6 = 0;
+    int cpt_total = 0;
+    for (int i=0; i<g.adjList.size(); i++){
+        if (g.adjList[i].size()==3){
+            cpt_3++;
+        }
+        if (g.adjList[i].size()==4){
+            cpt_4++;
+        }
+        if (g.adjList[i].size()==5){
+            cpt_5++;
+        }
+        if (g.adjList[i].size()==6){
+            cpt_6++;
+        }
+        cpt_total += 1;
+    }
+    std::cout << "Number of vertices with 3 neighbors: " << cpt_3 << std::endl;
+    std::cout << "Number of vertices with 4 neighbors: " << cpt_4 << std::endl;
+    std::cout << "Number of vertices with 5 neighbors: " << cpt_5 << std::endl;
+    std::cout << "Number of vertices with 6 neighbors: " << cpt_6 << std::endl;
+    std::cout << "Total number of vertices: " << cpt_total << std::endl;
+
+    
+
+    
 }
--- MIP constraints/MIP_model.hpp
+++ MIP constraints/MIP_model.hpp
@@ -3,15 +3,36 @@
 
 #include <vector>
 #include <queue>
+#include <unordered_map>
+#include <tuple>
+
+
+    struct KeyHash {
+    std::size_t operator()(const std::tuple<int, int, int>& key) const {
+        return std::hash<int>()(std::get<0>(key)) 
+                ^ std::hash<int>()(std::get<1>(key)) 
+                ^ std::hash<int>()(std::get<2>(key));
+        }
+    };
+
+
 
 class Graph {
     
 public:
-    Graph(int vertices); // Constructor
-    std::unordered_map<std::tuple<int, int, int>, int> mapping; // hashing using the mapping of 3D points
+    Graph(int N, int M, int L);// Constructor of the size of the Graph N*M*L
+    std::unordered_map<int, int> mapping; // hashing using the mapping of 3D points
     std::unordered_map<int, std::vector<int>> adjList; //adjacency list of the graph
     void addEdge(int src, int dest); // Function to add an edge to the graph
     void BFS(int startVertex); // BFS function
+    void CreateMapping(int N, int M, int L); // Create the mapping of the 3D points
+    void CreateGraph(int N, int M, int L); // Create the graph from the mapping of the 3D points
+    void printGraph(); // Print the graph
+    void printMapping(); // Print the mapping of the 3D points
+    int getIndex(int i, int j, int k, int N, int M, int L); // Get the index of the 3D point
+    std::tuple<int,int,int> getCoordinates(int index, int N, int M, int L); // Get the coordinates of the 3D point
+
+
 };
 
 #endif // BFS_HPP
